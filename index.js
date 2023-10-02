d925f03fadb18230f12fa29eae3166aa57d8acac
bunuabhi2023 <asraijada08@gmail.com>
2023-10-02 12:15:40 +0530

    cors resolve

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ app.use(
     cors({
         origin: [
             "http://localhost:3000",
+            "https://nyayamitra-geetanjalichawla.vercel.app"
           ],
           credentials: true,
     })
